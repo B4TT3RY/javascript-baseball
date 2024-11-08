094ef03d6553d78f342ebb9afaf01ffb28f2fb68
Lee Taehoon <gb4tt3ry@gmail.com>
2024-11-08 15:03:40 +0900

    Feat: 또 자리 교체

--- index.ts
+++ index.ts
@@ -3,37 +3,19 @@ import { stdin, stdout } from "process";
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
 
-// let input = "";
-
-// rl.on("line", (line) => {
-//   // console.log(line);
-//   input = line;
-//   console.log(input);
-//   rl.close();
-// });
-
 startGame();
-
-rl.on("close", () => {
-  process.exit();
-});
 
 async function startGame() {
   const result = await rl.question(
     "게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.\n"
   );
-  console.log("result: " + result);
 
   if (result === "1") {
     const randomNumber = getRandomNumber();
-    console.log("컴퓨터가 숫자를 뽑았습니다.");
-    rl.on("line", (answer: string) => {
-      if (answer === '2') {
-        endGame();
-      }
-      
-      compareAnswer(randomNumber, answer);
-    });
+    console.log("\n컴퓨터가 숫자를 뽑았습니다.\n");
+    console.log(`정답: ${randomNumber}`);
+
+    inputAnswer(randomNumber);
   } else if (result === "9") {
     endGame();
   }
@@ -47,25 +29,61 @@ function getRandomNumber(): string {
     set.add(randomNumber);
   }
 
-  const returnRandomNumber = Array.from(set).join('');
+  const returnRandomNumber = Array.from(set).join("");
   return returnRandomNumber;
 }
 
-function compareAnswer(randomNumber: string, answer: string) {
-  const strike = [...randomNumber].map((element, index) => {
-    if(answer[index] === element) return element;
-  }).filter((element) => element !== undefined).length;
+async function inputAnswer(randomNumber: string) {
+  const answer = await rl.question("숫자를 입력해주세요: ");
 
-  console.log(strike);
-  console.log(`test: ${randomNumber} ${answer}`)
+  if (answer === "2") {
+    endGame();
+  }
+
+  const isStrike = compareAnswer(randomNumber, answer);
+  if (isStrike) {
+    console.log("\n3개의 숫자를 모두 맞히셨습니다.");
+    console.log("-------게임 종료-------\n");
+    startGame();
+  } else {
+    inputAnswer(randomNumber);
+  }
+}
+
+function compareAnswer(randomNumber: string, answer: string): boolean {
+  const strike = [...randomNumber]
+    .map((element, index) => {
+      if (answer[index] === element) return element;
+    })
+    .filter((element) => element !== undefined).length;
+
+  const ball = [...answer]
+    .map((element, index) => {
+      if (randomNumber[index] !== element && randomNumber.includes(element))
+        return element;
+    })
+    .filter((element) => element !== undefined).length;
+
+  if (strike === 0 && ball > 0) {
+    console.log(`${ball}볼`);
+  } else if (strike > 0 && ball === 0) {
+    console.log(`${strike}스트라이크`);
+    if (strike === 3) {
+      return true;
+    }
+  } else if (strike > 0 && ball > 0) {
+    console.log(`${ball}볼 ${strike}스트라이크`);
+  } else {
+    console.log("낫싱");
+  }
+
+  return false;
 }
 
 function endGame() {
-  console.log("애플리케이션이 종료되었습니다.");
-  return rl.close();
+  console.log("\n애플리케이션이 종료되었습니다.");
+  process.exit();
 }
-
-
 
 /*
 예외처리 해야할 것
