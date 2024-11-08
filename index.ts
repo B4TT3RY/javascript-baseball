b906a8857dc6e6d6670234825cf6de5ee14ea827
Lee-chaerin <5543705@naver.com>
2024-11-08 14:31:15 +0900

    Feat: 자리교체

--- index.ts
+++ index.ts
@@ -25,34 +25,51 @@ async function startGame() {
   console.log("result: " + result);
 
   if (result === "1") {
-    getRandomNumber();
+    const randomNumber = getRandomNumber();
     console.log("컴퓨터가 숫자를 뽑았습니다.");
-    rl.on("line", (answer) => {
+    rl.on("line", (answer: string) => {
       if (answer === '2') {
         endGame();
       }
       
-      compareAnswer(answer);
+      compareAnswer(randomNumber, answer);
     });
   } else if (result === "9") {
     endGame();
   }
 }
 
-function getRandomNumber() {
+function getRandomNumber(): string {
   const set: Set<number> = new Set();
 
   while (set.size < 3) {
     const randomNumber = Math.floor(Math.random() * 9) + 1;
     set.add(randomNumber);
   }
+
+  const returnRandomNumber = Array.from(set).join('');
+  return returnRandomNumber;
 }
 
-function compareAnswer(answer: string) {
-  console.log(`test: ${answer}`)
+function compareAnswer(randomNumber: string, answer: string) {
+  const strike = [...randomNumber].map((element, index) => {
+    if(answer[index] === element) return element;
+  }).filter((element) => element !== undefined).length;
+
+  console.log(strike);
+  console.log(`test: ${randomNumber} ${answer}`)
 }
 
 function endGame() {
   console.log("애플리케이션이 종료되었습니다.");
   return rl.close();
 }
+
+
+
+/*
+예외처리 해야할 것
+1. 사용자 입력값 중복 X
+2. 숫자만 입력
+3. 3자리만 입력
+*/
