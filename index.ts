ec0925af69cc3c34d851d9af7bca1b173b34d766
Lee Taehoon <gb4tt3ry@gmail.com>
2024-11-16 21:52:25 +0900

    Style: 코드 포매팅

--- index.ts
+++ index.ts
@@ -20,8 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
 let state = State.StartGame; //시작 상태로 초기화
 let computerNumber: number[] = []; //컴퓨터 랜덤값
 
-
-
 /* ----- 채팅 ----- */
 //✔️알림창 추가
 const addAlert = (message: string) => {
@@ -63,51 +61,54 @@ const addUserChat = () => {
   handleUserInput(userInput);
 };
 
-
-
 /* ----- 게임 상태 ----- */
-const initializeGame = () => { //게임 초기화
+const initializeGame = () => {
+  //게임 초기화
   state = State.StartGame;
   addChat("computer", "게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.");
 };
 
-const startGame = (input: string) => { //게임 시작
-  if (input === State.StartGame) { //'1'일 경우
+const startGame = (input: string) => {
+  //게임 시작
+  if (input === State.StartGame) {
+    //'1'일 경우
     computerNumber = generateComputerNumber(THREE_DIGIT_NUMBER);
     addAlert("컴퓨터가 숫자를 뽑았습니다.");
     addChat("computer", "세자리 숫자를 입력해주세요.");
     state = State.RunningGame;
-  } else if (input === State.EndGame) { //'9'일 경우
+  } else if (input === State.EndGame) {
+    //'9'일 경우
     endGame();
   }
-}
+};
 
-const runningGame = (input: string) => { //게임중
-  if (input === State.RunningGame) { //'2'일 경우
-    return endGame(); 
+const runningGame = (input: string) => {
+  //게임중
+  if (input === State.RunningGame) {
+    //'2'일 경우
+    return endGame();
   }
 
-  let userNumber = convertUserInput(input);
+  const userNumber = convertUserInput(input);
   if (userNumber === null) {
     return addChat("computer", "잘못된 값을 입력했습니다.");
   }
-    
+
   const isWin = compareNumbers(computerNumber, userNumber); //반환값 true;
   if (isWin) {
     addChat("computer", "3개의 숫자를 모두 맞히셨습니다.");
     addAlert("게임 종료");
     initializeGame();
   }
-}
+};
 
-const endGame = () => { //게임 종료
+const endGame = () => {
+  //게임 종료
   state = State.EndGame;
   addAlert(
     "애플리케이션이 종료되었습니다.\n게임을 시작하시려면 새로고침 해주세요."
   );
 };
-
-
 
 /* ----- 게임 ----- */
 //✔️게임 흐름 제어
@@ -121,39 +122,54 @@ const handleUserInput = (input: string) => {
 
 //✔️랜덤 숫자 생성
 export const generateComputerNumber = (digitNumber: number): number[] => {
-  let shuffledNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const shuffledNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   shuffledNumbers.sort(() => Math.random() - 0.5);
   return shuffledNumbers.splice(0, digitNumber);
 };
 
 //✔️입력값 체크 후 변환한 값 반환
 export const convertUserInput = (input: string): number[] | null => {
-  if(!/^[1-9]+$/.test(input)) { //1~9 사이의 정수인지 체크
-     return null;
+  if (!/^[1-9]+$/.test(input)) {
+    //1~9 사이의 정수인지 체크
+    return null;
   }
-  if(input.length !== THREE_DIGIT_NUMBER) { //3자리인지 체크
+  if (input.length !== THREE_DIGIT_NUMBER) {
+    //3자리인지 체크
     return null;
   }
   const uniqueEntries = new Set(Array.from(input)); //중복값인지 체크
-  if(uniqueEntries.size !== THREE_DIGIT_NUMBER) {
+  if (uniqueEntries.size !== THREE_DIGIT_NUMBER) {
     return null;
   }
 
-  return Array.from(input).map(char => Number(char)); //숫자 배열 리턴
-}
+  return Array.from(input).map((char) => Number(char)); //숫자 배열 리턴
+};
 
 //✔️스트라이크 가져오기
-export const getStrikeCount = (computerNumber: number[], userNumber: number[]): number => {
-  return userNumber.filter((number, index) => computerNumber[index] === number).length;
+export const getStrikeCount = (
+  computerNumber: number[],
+  userNumber: number[]
+): number => {
+  return userNumber.filter((number, index) => computerNumber[index] === number)
+    .length;
 };
 
 //✔️볼 가져오기
-export const getBallCount = (computerNumber: number[], userNumber: number[]): number => {
-  return userNumber.filter((number, index) => computerNumber[index] !== number && computerNumber.includes(number)).length;
+export const getBallCount = (
+  computerNumber: number[],
+  userNumber: number[]
+): number => {
+  return userNumber.filter(
+    (number, index) =>
+      computerNumber[index] !== number && computerNumber.includes(number)
+  ).length;
 };
 
 //✔️값 비교
-export const compareNumbers = (computerNumber: number[], userNumber: number[]) => {
+export const compareNumbers = (
+  computerNumber: number[],
+  userNumber: number[]
+) => {
   const strikeCount = getStrikeCount(computerNumber, userNumber);
   const ballCount = getBallCount(computerNumber, userNumber);
 
