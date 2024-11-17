624c3335f88d77777a1593fb7f3d3fbb459103c2
znhxxn <ghoon7777@naver.com>
2024-11-17 23:49:43 +0900

    fix : statistic 추가

--- index.ts
+++ index.ts
@@ -1,25 +1,37 @@
-type SenderType = "computer" | "user"; //컴퓨터, 사람 도메인
-enum GameState { //진행 상태 도메인
+type Player = "computer" | "user"; // 컴퓨터, 사람 도메인
+enum GameState { // 진행 상태 도메인
   StartGame = "1",
   SettingGameRound = "SettingGameRound",
   RunningGame = "2",
   EndGame = "9",
-}
+};
+
+interface GameStatistic { // 게임 통계 기록
+  roundCount : number;
+  tryCount : number;
+  winner : Player;
+};
 
 interface GameStateStore {
   currentState: GameState; // 현재 진행 상태
   computerNumber: number[]; // 컴퓨터가 뽑은 숫자
   currentRound: number; // 현재 라운드
   roundCount: number; // 게임 횟수
-}
+  statistics : GameStatistic[]; // 게임 통계
+};
 
-const THREE_DIGIT_NUMBER = 3; //입력값 3자리로 제한
+const THREE_DIGIT_NUMBER = 3; // 입력값 3자리로 제한
 
 const store: GameStateStore = {
   currentState: GameState.StartGame,
   computerNumber: [],
   currentRound: 1,
   roundCount: 0,
+  statistics : []
+};
+
+const saveStatistics = (winner : Player) => {
+  
 };
 
 //페이지 시작할 때 initializeGame() 호출
@@ -45,7 +57,7 @@ const addAlert = (message: string) => {
 };
 
 //✔️채팅 추가
-const addChat = (sender: SenderType, message: string) => {
+const addChat = (sender: Player, message: string) => {
   const chat = document.createElement("p");
   if (sender === "computer") {
     chat.classList.add("computerChat");
