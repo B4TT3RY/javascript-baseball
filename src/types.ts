e83af53ecff75a9568df8c9cbd83ca6b84670bb0
Lee Taehoon <gb4tt3ry@gmail.com>
2024-11-22 22:24:14 +0900

    Feat: BallNumber 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,7 @@
+export type BallNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 export type Player = "computer" | "user" | "alert";
+
 export enum GameState {
   StartGame = "1",
   SettingGameRound = "SettingGameRound",
